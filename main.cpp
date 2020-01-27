30118bcdf3024b297c82a128c1def6fdfb9b5a0a
Sebastian Zborowski <58173641+401PWSZ@users.noreply.github.com>
2020-01-27 15:03:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -62,21 +62,9 @@ int main() {
     d2.galaz[0].dlugosc(3);
     d2.galaz[0].lisc2(70);
 
-
-
- //   cout << d1.nazwa() << " ma galezi: " << d1.galaz() << endl;
-//    cout << d2.nazwa() << " ma galezi: " << d2.galaz() << endl;
-/*    d1.galaz( new Galaz ); // dodaje pierwsza galaz
-    d1.galaz( new Galaz ); // dodaje kolejna galaz
-    Galaz* g1 = d1.galaz( 0 ); // pobieram pierwsza galaz
-    g1->dlugosc( 5 ); // ustawiam dlugosc galezi
-    g1->lisc( 100 ); // ustawiam liczbe lisci
-    Galaz* g2 = d2.galaz( 1 ); // pobieram druga galaz
-    g2->dlugosc( 3 ); // ustawiam dlugosc galezi
-    g2->lisc( 70 ); // ustawiam liczbe lisci
-*/
     cout << d1.nazwa() << " ma galezi: " << d1.rozmiar() << endl;
     cout << d2.nazwa() << " ma galezi: " << d2.rozmiar() << endl;
     cin.get();
 
 }
+//Sebastian Zborowski
