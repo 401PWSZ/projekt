0200e9a908d2c5201a7e81ac34abfeca6e367cab
Sebastian Zborowski <58173641+401PWSZ@users.noreply.github.com>
2020-01-27 15:00:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,110 +1,82 @@
-#include <iostream>  //bridge
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
 using namespace std;
 
-class DP1 {
+class Galaz {
     public:
-        void draw_a_line() {
-            cout << "Linia V1" << endl;
+        Galaz(){};
+        void dlugosc(double dlg){
+            double xx = dlg;
+            dlug = xx;
         }
-        void draw_a_circle() {
-            cout << "Okrag V1" << endl;
-        }
-};
-
-class DP2 {
-    public:
-        void drawline() {
-            cout << "Linia V2" << endl;
-        }
-        void drawcircle() {
-            cout << "Okrag V2" << endl;
-        }
-};
-
-class Drawing {
-    public:
-        virtual void drawLine() = 0;
-        virtual void drawCircle() = 0;
-        virtual ~Drawing() { }
-};
-
-class Shape {
-    public:
-        Shape( Drawing *p ) : ptr{p} { }
-        virtual ~Shape() { }
-        virtual void draw() = 0;
-        void setlib( Drawing* p) { ptr = p; }
-    protected:
-        void drawLine() { ptr->drawLine(); }
-        void drawCircle() { ptr->drawCircle(); }
-    private:
-        Drawing* ptr { nullptr };
-};
-
-class Circle : public Shape {
-    public:
-        using Shape::Shape; //wygeneruj konstruktor Circle na wzor Shape
-        void draw() {
-            cout << "Circle ";
-            drawCircle();
-        }
-};
-
-class Rectangle : public Shape {
-    public:
-        using Shape::Shape; //wygeneruj konstruktor Rectangle na wzor Shape
-        void draw() {
-            cout << "Prostokat ";
-            drawLine();
-        }
-};
-
-class V1Drawing : public Drawing {
-    public:
-        V1Drawing( DP1& r ) : ref(r) { }
-        void drawLine() {
-            ref.draw_a_line();
-        }
-        void drawCircle() {
-            ref.draw_a_circle();
+        void lisc2(int liczba){
+            size_t tak = liczba;
+            lisc = tak;
         }
     private:
-        DP1& ref;
+        double dlug = 0;
+        size_t lisc = 0;
+}; 
+
+class Drzewo {
+    public:
+        Drzewo( const char* n, double h ){
+			name = n;
+			height = h;
+			
+		};
+        string nazwa()
+        {
+
+            return name;
+        }
+
+        vector<Galaz> galaz;
+
+        unsigned int rozmiar()
+        {
+        unsigned int x = galaz.size();
+            return x;
+
+        }
+
+    private:
+        string name;
+        double height;
+        //vector<Galaz> galaz; // pamietac o skasowaniu obiektow
 };
 
-class V2Drawing : public Drawing {
-    public:
-        V2Drawing( DP2& r ) : ref(r) { }
-        void drawLine() {
-            ref.drawline();
-        }
-        void drawCircle() {
-            ref.drawcircle();
-        }
-    private:
-        DP2& ref;
-};
+
+
 int main() {
-    
-    DP1 library1; // pierwsza biblioteka graficzna
-    DP2 library2; // druga biblioteka graficzna
+    Drzewo d1("brzoza", 20); // nazwa wysokosc
+    Drzewo d2("sekwoja", 100); 
+    Galaz g1;
+    Galaz g2;
+    d1.galaz.push_back(g1);
+    d2.galaz.push_back(g2);
+    d1.galaz[0].dlugosc(5);
+    d1.galaz[0].lisc2(100);
+    d2.galaz[0].dlugosc(3);
+    d2.galaz[0].lisc2(70);
 
-    Drawing *d1 = new V1Drawing( library1 );
-    Drawing *d2 = new V2Drawing( library2 );
 
-    Shape *p1 = new Rectangle( d1 );
-    Shape *p2 = new Circle( d2 );
 
-    p1->draw(); // rectangle line V1
-    p2->draw(); // circle line V2
-    p1->setlib( d2 );
-    p2->setlib( d1 );
-    p1->draw(); // rectangle line V2
-    p2->draw(); // circle line V1
+ //   cout << d1.nazwa() << " ma galezi: " << d1.galaz() << endl;
+//    cout << d2.nazwa() << " ma galezi: " << d2.galaz() << endl;
+/*    d1.galaz( new Galaz ); // dodaje pierwsza galaz
+    d1.galaz( new Galaz ); // dodaje kolejna galaz
+    Galaz* g1 = d1.galaz( 0 ); // pobieram pierwsza galaz
+    g1->dlugosc( 5 ); // ustawiam dlugosc galezi
+    g1->lisc( 100 ); // ustawiam liczbe lisci
+    Galaz* g2 = d2.galaz( 1 ); // pobieram druga galaz
+    g2->dlugosc( 3 ); // ustawiam dlugosc galezi
+    g2->lisc( 70 ); // ustawiam liczbe lisci
+*/
+    cout << d1.nazwa() << " ma galezi: " << d1.rozmiar() << endl;
+    cout << d2.nazwa() << " ma galezi: " << d2.rozmiar() << endl;
+    cin.get();
 
-    delete p1;
-    delete p2;
-    delete d1;
-    delete d2;
-    
 }
